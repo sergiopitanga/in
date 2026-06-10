8079444c04a4883509c4104d7f6075c1aabc1c6b
dougdepaula <dougdepaula@outlook.com>
2026-06-10 16:40:37 -0300

    feat: corrige indexação da pasta frontend e remove submodule

---
+++ frontend/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import './style.css'
+import Home from './Pages/Home'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Home />)
