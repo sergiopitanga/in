1bbf302af04efaf362ee95e57bff9589895b791f
sergiopitanga <150822070+sergiopitanga@users.noreply.github.com>
2026-06-10 15:30:58 -0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import './style.css'
-import Home from './Pages/Home'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Home />)
